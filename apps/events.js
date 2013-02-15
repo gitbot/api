3984969411a97bcae55fd45524a57d7cc671efcb
Lakshmi Vyasarajan <lakshmi.vyas@gmail.com>
2013-02-15 18:24:45 +0530

    Redis pubsub further refactored.

--- apps/events.js
+++ apps/events.js
@@ -1,50 +1,63 @@
-var    config = require('../lib/config');
+var
+        config = require('../lib/config')
+    ,   redis = require('redis')
+    ,   sub = redis.createClient(config.db.port, config.db.host);
 
 module.exports.listen = function(app) {
 
-    var     io = require('socket.io').listen(app, {origins: config.socket.origins});
+    var
+            conf = {origins: config.socket.origins}
+        ,   userReady = ':user:ready'
+        ,   userReadyPattern = '*' + userReady
+        ,   projectReady = ':project:ready'
+        ,   projectReadyPattern = '*' + projectReady
+        ,   io = require('socket.io').listen(app, conf)
+        ,   socketMap = {};
+
+    var emit = function(channel, suffix, message) {
+        var username = channel.replace(suffix, '');
+        var sockets = socketMap[username];
+        if (sockets) {
+            for (var socket in sockets) {
+                socket.volatile.emit(message);
+            }
+        }
+    };
+    
+    var pubsub = function() {
+        sub.on("end", function () {
+            console.log('Redis disconnected.');
+            sub = redis.createClient(config.db.port, config.db.host);
+            pubsub();
+        }).on("error", function (err) {
+            console.log('Redis error.');
+            console.error(err);
+        }).on("connect", function() {
+            console.log('Redis connected.');
+            console.log('Subscribing to ' + userReady);
+            sub.psubscribe('*' + userReady);
+            console.log('Subscribing to ' + projectReady);
+            sub.psubscribe('*' + projectReady);
+        }).on("pmessage", function(pattern, channel, message) {
+            console.log('Message received: channel=' + channel +
+                                ',msg=' + message);
+            if (pattern === userReadyPattern) {
+                emit(channel, userReady, 'ready');
+            } else if (pattern === projectReady) {
+                emit(channel, projectReady, 'ready');
+            }
+        });
+
+    };
+
+    pubsub();
 
     io.sockets.on('connection', function(socket) {
-        var     userReady = '*:user:ready'
-            ,   projectReady = '*:project:ready'
-            ,   data = { username: null }
-            ,   self = this
-            ,   redis = null;
-
-        var pubsub = function() {
-
-            redis = require('redis').createClient(
-                            config.db.port, config.db.host);
-
-            redis.on("end", function () {
-                console.log('Redis disconnected.');
-                pubsub();
-            }).on("error", function (err) {
-                console.log('Redis error.');
-                console.error(err);
-            }).on("connect", function() {
-                console.log('Redis connected.');
-                console.log('Subscribing to ' + userReady);
-                redis.psubscribe(userReady);
-                console.log('Subscribing to ' + projectReady);
-                redis.psubscribe(projectReady);
-            }).on("pmessage", function(pattern, channel, message) {
-                console.log('Message received: channel=' + channel +
-                                    ',msg=' + message);
-                if (pattern === userReady &&
-                    channel === userReady.replace('*', data.username)) {
-                    socket.volatile.emit('ready');
-                } else if (pattern === projectReady &&
-                    channel === userReady.replace('*', data.username)) {
-                    socket.volatile.emit('projectReady');
-                }
-            });
-
-        };
-        pubsub();
         socket.on("init", function(username) {
             console.log("Got init event");
-            data.username = username;
+            var userSockets = socketMap[username] || [];
+            userSockets.push(socket);
+            socketMap[username] = userSockets;
         });
         socket.on("disconnect", function() {
             console.log("Socket disconnected");
