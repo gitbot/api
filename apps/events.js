80b59bc556048c4d16d1f3396a26e13bb89937c8
Lakshmi Vyasarajan <lakshmi.vyas@gmail.com>
2013-02-15 17:57:25 +0530

    Refactor redis handling.

--- apps/events.js
+++ apps/events.js
@@ -7,34 +7,49 @@ module.exports.listen = function(app) {
     io.sockets.on('connection', function(socket) {
         var     userReady = '*:user:ready'
             ,   projectReady = '*:project:ready'
-            ,   redis = require('redis').createClient(config.db.port, config.db.host);
+            ,   data = { username: null }
+            ,   self = this
+            ,   redis = null;
 
-        redis.on("error", function (err) {
-            console.log('Redis error.');
-            console.error(err);
-        }).on("connect", function() {
-            console.log('Redis connected.');
-            console.log('Subscribing to ' + userReady);
-            redis.psubscribe(userReady);
-            console.log('Subscribing to ' + projectReady);
-            redis.psubscribe(projectReady);
-        });
-        socket.on("init", function(username) {
-            console.log("Got init event");
-            redis.on("pmessage", function(pattern, channel, message) {
-                console.log('Message received: channel=' + channel + ',msg=' + message);
+        var pubsub = function() {
+
+            redis = require('redis').createClient(
+                            config.db.port, config.db.host);
+
+            redis.on("end", function () {
+                console.log('Redis disconnected.');
+                pubsub();
+            }).on("error", function (err) {
+                console.log('Redis error.');
+                console.error(err);
+            }).on("connect", function() {
+                console.log('Redis connected.');
+                console.log('Subscribing to ' + userReady);
+                redis.psubscribe(userReady);
+                console.log('Subscribing to ' + projectReady);
+                redis.psubscribe(projectReady);
+            }).on("pmessage", function(pattern, channel, message) {
+                console.log('Message received: channel=' + channel +
+                                    ',msg=' + message);
                 if (pattern === userReady &&
-                    channel === userReady.replace('*', username)) {
+                    channel === userReady.replace('*', data.username)) {
                     socket.volatile.emit('ready');
                 } else if (pattern === projectReady &&
-                    channel === userReady.replace('*', username)) {
+                    channel === userReady.replace('*', data.username)) {
                     socket.volatile.emit('projectReady');
                 }
             });
+
+        };
+        socket.on("init", function(username) {
+            console.log("Got init event");
+            data.username = username;
         });
         socket.on("disconnect", function() {
             console.log("Socket disconnected");
-            redis.quit();
+            if (redis) {
+                redis.quit();
+            }
         });
     });
 };
